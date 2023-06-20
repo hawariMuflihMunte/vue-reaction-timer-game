761b7d8239c4d234ed0083d77b60b62511729df9
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-06-20 20:27:55 +0700

    Finished `Lifecycle Hooks` segment

--- reaction-timer/src/components/Block.vue
+++ reaction-timer/src/components/Block.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="block">
+    <div class="block" v-if="showBlock">
       Click Me!
     </div>
 </template>
@@ -7,7 +7,19 @@
 
 <script>
 export default {
-  props: ['delay']
+  props: ['delay'],
+  data() {
+    return {
+      showBlock: false
+    }
+  },
+  mounted() {
+    // console.log('Component mounted')
+    setTimeout(() => {
+      this.showBlock = true
+      // console.log(this.delay);
+    }, this.delay);
+  }
 }
 </script>
 
